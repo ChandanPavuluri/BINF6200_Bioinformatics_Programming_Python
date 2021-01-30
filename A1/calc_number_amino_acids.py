5afc728e783b30a71c5998e2d91238411a2bc198
ChandanPavuluri <77353407+ChandanPavuluri@users.noreply.github.com>
2021-01-30 14:36:06 -0500

    Update calc_number_amino_acids.py

--- A1/calc_number_amino_acids.py
+++ A1/calc_number_amino_acids.py
@@ -3,19 +3,28 @@
 #Calc_number_amino_acids.py
 """
 import sys
+def my_function():
+    # Input- Entering the name of DNA sequence
+    seq_name = str(input("Please enter a name for the DNA sequence: "))
+    #Printing the name of sequence
+    print("Your sequence name is:", seq_name)
+    # Input- Entering the length of sequence
+    seq_length = int(input("Please enter the length of the sequence: "))
+    # If-else Condition to check if the sequence is divisible by 3
+    if seq_length % 3 == 0:
+        # If sequence is divisible by 3 prints the sequence length
+        print("The length of the DNA sequence is:", seq_length)
+        # Dividing the sequence by 3
+        protein = (seq_length/3)
+        # Printing the length of the protein
+        print("The length of the decoded protein is:", float(protein))
+        # Printing the average weight of the sequence
+        print(f"The average weight of protein sequnce is: {(protein*110)/1000:0.2f}")
 
-SEQ_NAME = str(input("Please enter a name for the DNA sequence: "))
+    else:
+        # If the sequence is not a divisible of 3 it exits
+        print('Error: the DNA sequence is not a multiple of 3"')
+        sys.exit()
 
-print("Your sequence name is:", SEQ_NAME)
-
-SEQ_LENGTH = int(input("Please enter the length of the sequence: "))
-
-if SEQ_LENGTH % 3 == 0:
-    print("The length of the DNA sequence is:", SEQ_LENGTH)
-    PROTEIN = (SEQ_LENGTH/3)
-    print("The length of the decoded protein is:", float(PROTEIN))
-    print(f"The average weight of protein sequnce is: {(PROTEIN*110)/1000:0.2f}")
-
-else:
-    print('Error: the DNA sequence is not a multiple of 3"')
-    sys.exit()
+if __name__ == "__main__":
+    my_function()
