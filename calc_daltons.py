73b8fead6ee99d7a880faf0879961c6ee62e1626
ChandanPavuluri <77353407+ChandanPavuluri@users.noreply.github.com>
2021-01-30 14:20:54 -0500

    Update calc_daltons.py

--- calc_daltons.py
+++ calc_daltons.py
@@ -15,19 +15,16 @@ EYVNGGDLMYHIQQVGRFKEPHAVFYAAEIAIGLFFLQSKGIIYRDLKLDNVMLDSEGHIKIADFGMCKE
 NIWDGVTTKTFCGTPDYIAPEIIAYQPYGKSVDWWAFGVLLYEMLAGQAPFEGEDEDELFQSIMEHNVAY
 PKSMSKEAVAICKGLMTKHPGKRLGCGPEGERDIKEHAFFRYIDWEKLERKEIQPPYKPKARDKRDTSNF
 DKEFTRQPVELTPTDKLFIMNLDQNEFAGFSYTNPEFVINV'''
-    
-    # to get rid of newline characters
+    # To get rid of newline characters
     protein = protein.replace('\r', "").replace('\n', "")
-    
-    
+    # Calculate the length of Sequence    
     amino_acids = len(protein)
-
+    # Prints the statement
     print('The length of "Protein kinase C beta type" is:', str(amino_acids))
-
+    # Calculate the average weight
     average_weight = (amino_acids * 110) / 1000
-
+    # Prints the statement
     print('The average weight of this protein sequence in kilodaltons is:', str(average_weight))
-
 
 if __name__ == "__main__":
     my_function()
