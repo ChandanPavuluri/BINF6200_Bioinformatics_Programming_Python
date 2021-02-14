707202e0bc60c06ff878b8a31d2c9684372179ce
ChandanPavuluri <chandan@chandan-ideapad-110.home>
2021-02-14 17:23:25 -0500

    UPDATED

--- A2/descriptive_statistics.py
+++ A2/descriptive_statistics.py
@@ -2,46 +2,61 @@
 Descriptive Statistics and Lists
 #descriptive_statistics.py
 """
+#importing required modules(sys and math)
+
 import sys
 import math
 
 def min_max():
     """Function for identifying Maximum and Minimum number"""
-    column_data.sort()
-    minimum = column_data[0]
-    maximum = column_data[-1]
+
+    #sorting the list in ascending order and selecting first and last values
+    COLUMN_DATA.sort()
+    minimum = COLUMN_DATA[0]
+    maximum = COLUMN_DATA[-1]
+
+    #Printing maximum and minumum values
     print("{:<10}{}{:>10.3f}".format("Maximum", "=", maximum))
     print("{:<10}{}{:>10.3f}".format("Minimum", "=", minimum))
 
 def variance_stddev():
     """Function for calculating Variance and Standard Deviation"""
+
+    #Calculating the variance
     var = []
-    for number in column_data:
-        var.append((number - Average) ** 2)
+    for number in COLUMN_DATA:
+        var.append((number - AVERAGE) ** 2)
     try:
-        variance = sum(var) / (ValidNum - 1)
+        variance = sum(var) / (VALIDNUM - 1)
     except ZeroDivisionError:
         variance = 0
     print("{:<10}{}{:>10.3f}".format("Variance", "=", variance))
+
+    #Calculating Standard Deviation
     std_dev = math.sqrt(variance)
     print("{:<10}{}{:>10.3f}".format("Std_Dev", "=", std_dev))
 
 def statistics():
     """Function for calculating Median"""
-    column_data.sort()
+
+    #sorting the list in ascending order
+    COLUMN_DATA.sort()
+
     # Finding the position of the median
-    if len(column_data) % 2 == 0:
-        first_value = column_data[len(column_data) // 2]
-        second_value = column_data[len(column_data) // 2 - 1]
+    if len(COLUMN_DATA) % 2 == 0:
+        first_value = COLUMN_DATA[len(COLUMN_DATA) // 2]
+        second_value = COLUMN_DATA[len(COLUMN_DATA) // 2 - 1]
         median = (first_value + second_value) / 2
     else:
-        median = column_data[len(column_data) // 2]
+        median = COLUMN_DATA[len(COLUMN_DATA) // 2]
     print("{:<10}{}{:>10.3f}".format("Median", "=", median))
 
 if __name__ == "__main__":
-    arg_count = len(sys.argv) - 1
+
+    ARG_COUNT = len(sys.argv) - 1
+
     # if length of the argument count was less than 2 we need to raise an exception
-    if arg_count < 2:
+    if ARG_COUNT < 2:
         raise Exception("This script requires 2 arguments: Datafile name and then column number")
 
     INPUT_DATA_FILE = sys.argv[1]
@@ -49,7 +64,7 @@ if __name__ == "__main__":
 
     DATA = []
     COLUMN_DATA = []
-    nan = []
+    NAN = []
 
     with open(INPUT_DATA_FILE) as input_file:
         for line in input_file:
@@ -62,38 +77,38 @@ if __name__ == "__main__":
                 print("Exiting: There is no valid 'list index' in column {} in line {} in file: {}"
                       .format(COLUMN_NUMBER, i, INPUT_DATA_FILE))
                 sys.exit()
-    removed_value = 0
+    REMOVED_VALUE = 0
     for index, value in enumerate(DATA):
         try:
             if value in ("NaN", "nan"):
-                nan.append(value)
-                removed_value += 1
+                NAN.append(value)
+                REMOVED_VALUE += 1
             else:
                 COLUMN_DATA.append(float(value))
         except ValueError:
             print("Skipping line number {} : could not convert string to float: '{}'"
                   .format(index + 1, value))
-            removed_value += 1
+            REMOVED_VALUE += 1
 
     # Calculating length of total column
-    Count = len(COLUMN_DATA) + removed_value
+    COUNT = len(COLUMN_DATA) + REMOVED_VALUE
 
     # Calculating length of valid Numbers
-    ValidNum = len(COLUMN_DATA)
+    VALIDNUM = len(COLUMN_DATA)
 
     #Calculating Average
     try:
-        Average = sum(COLUMN_DATA) / ValidNum
+        AVERAGE = sum(COLUMN_DATA) / VALIDNUM
     except ZeroDivisionError:
         print("Error: There were no valid number(s) in column {} in file: {}"
-              .format(COLUMN_NUMBER, DATA_FILE))
+              .format(COLUMN_NUMBER, INPUT_DATA_FILE))
         sys.exit()
 
 
     print("{:<4} {}".format("Column:", COLUMN_NUMBER))
-    print("{:<10}{}{:>10.3f}".format("Count", "=", Count))
-    print("{:<10}{}{:>10.3f}".format("ValidNum", "=", ValidNum))
-    print("{:<10}{}{:>10.3f}".format("Average", "=", Average))
+    print("{:<10}{}{:>10.3f}".format("Count", "=", COUNT))
+    print("{:<10}{}{:>10.3f}".format("ValidNum", "=", VALIDNUM))
+    print("{:<10}{}{:>10.3f}".format("Average", "=", AVERAGE))
     min_max()
     variance_stddev()
     statistics()
