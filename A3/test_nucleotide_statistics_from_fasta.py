ecef644e5c0ef1b83c62fbb08d08dab9113835f9
ChandanPavuluri <chandan@chandan-ideapad-110.home>
2021-03-05 23:21:37 -0500

    MAR 5

--- A3/test_nucleotide_statistics_from_fasta.py
+++ A3/test_nucleotide_statistics_from_fasta.py
@@ -1,5 +1,47 @@
+"""Test suite for nucleotide_statistics_from_fasta.py"""
+
+# Importing the required modeules
+import os
 import pytest
-from nucleotide_statistics_from_fasta import get_fh, get_header_and_sequence_lists, print_sequence_stats
+
+
+# importing functions from program file to test the program
+from nucleotide_statistics_from_fasta import get_fh, get_header_and_sequence_lists, \
+        _get_accession, _get_nt_occurrence, print_sequence_stats
+
+# ignore all "Missing function or method docstring" since this is a unit test
+# pylint: disable=C0116
+# ignore all "function name "test_get_fh_IOError" doesn't conform to snake_case naming style
+# pylint: disable=C0103
+
+FILE_2_TEST = "test.txt"
+
+FASTA_STRING = """\
+>E123 sequence
+ATGCTAGCTA
+"""
+
+TEST_FILE = open("fasta.txt", "w")
+TEST_FILE.write(FASTA_STRING)
+TEST_FILE.close()
+
+
+def _create_test_file(file):
+    # not actually run, helper function for test script
+    # create a test file
+    open(file, "w").close()
+
+
+def test_existing_get_fh_4_reading():
+    # does it open file for reading
+    # create a test file
+    _create_test_file(FILE_2_TEST)
+
+    # test
+    test = get_fh(FILE_2_TEST, "r")
+    assert hasattr(test, "readline") is True, "Not able to open for reading"
+    test.close()
+    os.remove(FILE_2_TEST)
 
 
 def test_get_fh_4_IOError():
@@ -10,11 +52,26 @@ def test_get_fh_4_IOError():
 
 
 def test_get_header_and_sequence_lists():
-    fh_in = get_fh("test.txt", "r")
-    assert get_header_and_sequence_lists(fh_in) == (['>1 sequence'], ['ATGCTAGCTA'])
+    # does it return the lists
+    fh_in = get_fh("fasta.txt", "r")
+    assert get_header_and_sequence_lists(fh_in) == (['>E123 sequence'], ['ATGCTAGCTA'])
+
+
+def test__get_accession__get_nt_occurrence():
+    # does it correctly cuts the header to give accession number
+    fh_in = get_fh("fasta.txt", "r")
+    list_headers, list_seqs = get_header_and_sequence_lists(fh_in)
+    assert _get_accession(list_headers[0]) == "E123"
+    assert _get_nt_occurrence("A", list_seqs[0]) == 3
+
 
 def test_print_sequence_stats():
-    fh_in = get_fh("test.txt", "r")
+    # does it correctly return the stats
+    fh_in = get_fh("fasta.txt", "r")
     list_headers, list_seqs = get_header_and_sequence_lists(fh_in)
     fh_out = get_fh("output.txt", "w")
-    print_sequence_stats(list_headers, list_seqs, fh_out) == (["1", "1", "3", "2", "2", "3", "0", "10", "40.0"])
+    assert print_sequence_stats(list_headers, list_seqs, fh_out) == \
+            (["1", "E123", "3", "2", "2", "3", "0", "10", "40.0"])
+    os.remove("output.txt")
+
+os.remove("fasta.txt")
--- A3/test_pdb_fasta_splitter.py
+++ A3/test_pdb_fasta_splitter.py
@@ -1,5 +1,17 @@
 import pytest
+import os
 from pdb_fasta_splitter import get_fh, get_header_and_sequence_lists, file_write
+
+
+
+FASTA_STRING = """\
+>E123 sequence
+ATGCTAGCTA
+"""
+
+TEST_FILE = open("fasta.txt","w")
+TEST_FILE.write(FASTA_STRING)
+TEST_FILE.close()
 
 
 def test_get_fh_4_IOError():
@@ -11,10 +23,16 @@ def test_get_fh_4_IOError():
 
 
 def test_get_header_and_sequence_lists():
-    fh_in = get_fh("test.txt", "r")
-    assert get_header_and_sequence_lists(fh_in) == (['>1 sequence'], ['ATGCTAGCTA'])
+    fh_in = get_fh("fasta.txt", "r")
+    assert get_header_and_sequence_lists(fh_in) == (['>E123 sequence'], ['ATGCTAGCTA'])
 
 def test_file_write():
-    fh_in = get_fh("test.txt", "r")
+    fh_in = get_fh("fasta.txt", "r")
     list_headers, list_seqs = get_header_and_sequence_lists(fh_in)
     assert file_write(list_headers, list_seqs) == (1,0)
+    os.remove("pdb_protein.fasta")
+    os.remove("pdb_ss.fasta")
+    os.remove("fasta.txt")
+
+
+
