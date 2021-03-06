784e3f1ca7cd9b5f21a94242e7f1324b6d29501d
ChandanPavuluri <77353407+ChandanPavuluri@users.noreply.github.com>
2021-03-05 23:26:51 -0500

    Update test_pdb_fasta_splitter.py

--- A3/test_pdb_fasta_splitter.py
+++ A3/test_pdb_fasta_splitter.py
@@ -1,8 +1,17 @@
-import pytest
+"""Test suite for pdb_fasta_splitter.py"""
+
+# Importing the required modeules
 import os
+import pytest
+
+
+# importing functions from program file to test the program
 from pdb_fasta_splitter import get_fh, get_header_and_sequence_lists, file_write
 
-
+# ignore all "Missing function or method docstring" since this is a unit test
+# pylint: disable=C0116
+# ignore all "function name "test_get_fh_IOError" doesn't conform to snake_case naming style
+# pylint: disable=C0103
 
 FASTA_STRING = """\
 >E123 sequence
@@ -23,10 +32,12 @@ def test_get_fh_4_IOError():
 
 
 def test_get_header_and_sequence_lists():
+    # does it return the lists
     fh_in = get_fh("fasta.txt", "r")
     assert get_header_and_sequence_lists(fh_in) == (['>E123 sequence'], ['ATGCTAGCTA'])
 
 def test_file_write():
+    # does it give the exact count and write in to the files
     fh_in = get_fh("fasta.txt", "r")
     list_headers, list_seqs = get_header_and_sequence_lists(fh_in)
     assert file_write(list_headers, list_seqs) == (1,0)
