11c77162c5b453483bc11b9f6804e9c99332a6d2
ChandanPavuluri <ckpavuluri@gmail.com>
2021-03-17 23:21:18 -0400

    Commented and test cases

---
+++ A4/tests/unit/test_my_io.py
@@ -0,0 +1,49 @@
+import os
+import pytest
+
+# ignore all "Missing function or method docstring" since this is a unit test
+# pylint: disable=C0116
+# ignore all "Function name "test_get_fh_4_IOError" doesn't conform to snake_case naming style"
+# pylint: disable=C0103
+
+FILE_2_TEST = "test.txt"
+
+def _create_test_file(file):
+    # not actually run, the are just helper functions for the test script
+    # create a test file
+    open(file, "w").close()
+
+def test_existing_get_fh_4_reading():
+   # does it open a file for reading
+   # create a test file
+    create_test_file(FILE_2_TEST)
+    # test
+    test = get_fh(FILE_2_TEST, "r")
+    assert hasattr(test, "readline") is True, "Not able to open for reading"
+    test.close()
+    os.remove(FILE_2_TEST)
+
+
+def test_existing_get_fh_4_writing():
+    # does it open a file for writing
+    # test
+    test = get_fh(FILE_2_TEST, "w")
+    assert hasattr(test, "write") is True, "Not able to open for writing"
+    test.close()
+    os.remove(FILE_2_TEST)
+
+
+def test_get_fh_4_IOError():
+    # does it raise IOError
+    # this should exit
+    with pytest.raises(IOError):
+        get_fh("does_not_exist.txt", "r")
+
+
+def test_get_fh_4_ValueError():
+    # does it raise ValueError
+    # this should exit
+    _create_test_file(FILE_2_TEST)
+     with pytest.raises(ValueError):
+        get_fh("does_not_exist.txt", "rrr")
+        os.remove(FILE_2_TEST)
